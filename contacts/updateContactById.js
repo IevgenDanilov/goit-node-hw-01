f1a9b2bd281c3b2bf2b6c7320e47d1393337dd69
Yevhen Danilov <danilov.evgen@gmail.com>
2021-11-12 11:57:20 +0200

    hw1 compleated upd

--- contacts/updateContactById.js
+++ contacts/updateContactById.js
@@ -3,14 +3,13 @@ const updateСontacts = require("./updateСontacts");
 
 const updateContactById = async (id, data) => {
   const contacts = await listContacts();
-  const idx = contacts.findIndex((contact) => contact.id === id);
+  const idx = contacts.findIndex((contact) => contact.id === Number(id));
   if (idx === -1) {
     return null;
   }
-  contacts[idx] = data;
+  contacts[idx] = { id, ...data };
   await updateСontacts(contacts);
   return contacts[idx];
-  // return contacts[id];
 };
 
 module.exports = updateContactById;
